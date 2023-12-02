1b600351206e03cd374ee740eb9a54c9947d30d9
Ivo Leist <ivo.leist@googlemail.com>
2023-12-02 14:55:08 +0100

    extended the script so it in the end is also building the newest docker image

--- build/build_phenoRankeR_w_docker.sh
+++ build/build_phenoRankeR_w_docker.sh
@@ -14,7 +14,7 @@ cd ../
 
 echo Build the docker image...
 DB_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' pheno-ranker-db) 
-docker build -f Dockerfile_package_builder --build-arg DB_IP=$${DB_IP} -t phenorankerui_package_builder:latest .
+# docker build -f Dockerfile_package_builder --build-arg DB_IP=$${DB_IP} -t phenorankerui_package_builder:latest .
 
 echo "Starting container..."
 container_id=$(docker run -d $IMAGE_NAME)
@@ -28,10 +28,15 @@ docker cp "${container_id}:${CONTAINER_PATH}/." $HOST_PATH/$new_dir
 docker wait $container_id
 
 echo "Stopping and removing container..."
-docker stop $CONTAINER_NAME
-docker rm $CONTAINER_NAME
+docker stop $container_id
+docker rm $container_id
 
 echo "Copying build package to phenoRankeR/deploy..."
 cp $HOST_PATH/$new_dir/*.tar.gz phenoRankeR/deploy/.
+
+# build the newest docker image
+phenoRankeR_VERSION=$(ls $HOST_PATH/$new_dir/*.tar.gz | sed 's/.*_\(.*\)\.tar\.gz/\1/')
+echo "Building docker image with version: $phenoRankeR_VERSION"
+cd phenoRankeR/deploy && docker build -f Dockerfile -t phenorankerui:${phenoRankeR_VERSION} .
 
 echo "Process completed."
