5dbf9e1d97f4aceee03f529e2d0d152e383911b1
Ivo Leist <ivo.leist@googlemail.com>
2024-02-21 15:25:18 +0100

    try to implement the navbar as a module

---
+++ phenoRankeR/R/mod_navbar.R
@@ -0,0 +1,12 @@
+#' Landing Page Module
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom gridlayout grid_container grid_card grid_place
+#' @importFrom bslib card_header card_body
+#' @importFrom shiny NS actionButton
+
