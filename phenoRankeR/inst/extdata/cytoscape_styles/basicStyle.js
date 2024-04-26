cee6c1fe975103e6680a7117df8ea22d2f2a212f
Ivo Leist <ivo.leist@googlemail.com>
2024-04-26 21:01:41 +0200

    improved basicStyle.js

--- phenoRankeR/inst/extdata/cytoscape_styles/basicStyle.js
+++ phenoRankeR/inst/extdata/cytoscape_styles/basicStyle.js
@@ -1,34 +1,7 @@
 [
   {
-    "selector": "node[color='red']", "css": {
-      "border-color": "red",
-      "content": "data(id)",
-      "border-width": "3px",
-      "width": "40px",
-      "height": "40px"
-    }
-  },
-  {
-    "selector": "node[color='blue']", "css": {
-      "border-color": "blue",
-      "content": "data(id)",
-      "border-width": "3px",
-      "width": "40px",
-      "height": "40px"
-    }
-  },
-  {
-    "selector": "node[color='green']", "css": {
-      "border-color": "green",
-      "content": "data(id)",
-      "border-width": "3px",
-      "width": "40px",
-      "height": "40px"
-    }
-  },
-  {
-    "selector": "node[color='black']", "css": {
-      "border-color": "black",
+    "selector": "node", "css": {
+      "border-color": "data(color)",
       "content": "data(id)",
       "border-width": "3px",
       "width": "40px",
@@ -37,30 +10,9 @@
   },
   {
     "selector": "edge", "css": {
-      "line-color": "magenta",
+      "line-color": "data(color)",
       "target-arrow-shape": "triangle",
-      "target-arrow-color": "magenta"
-    }
-  },
-  {
-    "selector": "edge[color='red']", "css": {
-      "line-color": "red",
-      "target-arrow-shape": "triangle",
-      "target-arrow-color": "red"
-    }
-  },
-  {
-    "selector": "edge[color='blue']", "css": {
-      "line-color": "blue",
-      "target-arrow-shape": "triangle",
-      "target-arrow-color": "blue"
-    }
-  },
-  {
-    "selector": "edge[color='green']", "css": {
-      "line-color": "green",
-      "target-arrow-shape": "triangle",
-      "target-arrow-color": "green"
+      "target-arrow-color": "data(color)"
     }
   }
 ]
