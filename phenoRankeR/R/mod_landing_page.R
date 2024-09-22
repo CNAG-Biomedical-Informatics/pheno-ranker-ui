da4f9bba3b34db10f4822ea830e5505f2036fe16
Ivo Leist <ivo.leist@googlemail.com>
2024-09-22 13:47:37 +0200

    codepen flowchart v1

--- phenoRankeR/R/mod_landing_page.R
+++ phenoRankeR/R/mod_landing_page.R
@@ -205,166 +205,277 @@ mod_landing_page_ui <- function(id) {
       card_body(
         tags$head(
           tags$style(HTML("
-          .flowchart-container {
+          .flowchart {
+            background-color: white;
+            border-radius: 15px;
+            box-shadow: 0 10px 30px rgba(0, 0, 0, 0.1);
+            padding: 30px;
+            max-width: 900px;
+            width: 100%;
+            text-align: center;
             position: relative;
-            width: 800px;
-            height: 600px;
-            margin: 0 auto;
           }
-          .circle {
-            width: 100px;
-            height: 100px;
-            background-color: lightgreen;
-            border-radius: 50%;
-            position: absolute;
-            text-align: center;
-            line-height: 100px;
-            font-weight: bold;
-          }
-          .rectangle {
-            width: 150px;
-            height: 60px;
-            background-color: lightgrey;
-            position: absolute;
-            text-align: center;
-            line-height: 60px;
-            font-weight: bold;
-          }
-          .diamond {
-            width: 100px;
-            height: 100px;
-            background-color: lightgoldenrodyellow;
-            position: absolute;
-            transform: rotate(45deg);
-            text-align: center;
-            line-height: 100px;
-            font-weight: bold;
-          }
-          .diamond-text {
+          .tree ul {
+            padding-top: 20px;
             position: relative;
-            top: 25%;
-            transform: rotate(-45deg);
-            width: 100px;
-            height: 50px;
-            margin: 0 auto;
+            transition: all 0.5s;
+            display: flex;
+            justify-content: center;
           }
-          .line {
+          .tree li {
+            display: inline-block;
+            text-align: center;
+            list-style-type: none;
+            position: relative;
+            padding: 20px 5px 0 5px;
+            transition: all 0.5s;
+          }
+          .tree li::before,
+          .tree li::after {
+            content: '';
             position: absolute;
-            background-color: black;
+            top: 0;
+            right: 50%;
+            border-top: 1px solid #ccc;
+            width: 50%;
+            height: 20px;
           }
-          .line.vertical {
-            width: 2px;
+          .tree li::after {
+            right: auto;
+            left: 50%;
+            border-left: 1px solid #ccc;
           }
-          .line.horizontal {
-            height: 2px;
+          .tree li:only-child::after,
+          .tree li:only-child::before {
+            display: none;
           }
-          .arrow {
+          .tree li:only-child {
+            padding-top: 0;
+          }
+          .tree li:first-child::before,
+          .tree li:last-child::after {
+            border: 0 none;
+          }
+          .tree li:last-child::before {
+            border-right: 1px solid #ccc;
+            border-radius: 0 5px 0 0;
+          }
+          .tree li:first-child::after {
+            border-radius: 5px 0 0 0;
+          }
+          .tree ul ul::before {
+            content: '';
             position: absolute;
+            top: 0;
+            left: 50%;
+            border-left: 1px solid #ccc;
             width: 0;
-            height: 0;
+            height: 20px;
           }
-          .arrow.down {
-            border-left: 10px solid transparent;
-            border-right: 10px solid transparent;
-            border-top: 15px solid black;
+          .tree li a {
+            border: 1px solid #ccc;
+            padding: 10px 15px;
+            text-decoration: none;
+            color: #666;
+            font-size: 14px;
+            display: inline-block;
+            border-radius: 5px;
+            transition: all 0.3s ease;
+            background-color: #e3f2fd;
+            color: #1565c0;
+            position: relative;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
           }
-          .arrow.up {
-            border-left: 10px solid transparent;
-            border-right: 10px solid transparent;
-            border-bottom: 15px solid black;
+          .tree li a:hover {
+            background-color: #c8e4f8;
+            color: #000;
+            border: 1px solid #94a0b4;
           }
-          .arrow.right {
-            border-top: 10px solid transparent;
-            border-bottom: 10px solid transparent;
-            border-left: 15px solid black;
+          .icon {
+            font-size: 20px;
+            margin-right: 5px;
           }
-          .arrow.left {
-            border-top: 10px solid transparent;
-            border-bottom: 10px solid transparent;
-            border-right: 15px solid black;
+          .decision {
+            background-color: #fff9c4;
+            color: #f57f17;
+            padding: 10px 15px;
+          }
+          .process {
+            background-color: #e3f2fd;
+            color: #1565c0;
+            padding: 10px 15px;
           }
           .label {
             position: absolute;
-            background-color: white;
+            background-color: #fff;
             padding: 2px 5px;
-            font-weight: bold;
+            font-size: 12px;
+            color: #333;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            z-index: 999;
           }
-        "))
+          .label-no {
+            right: 20%; 
+            top: -5%;
+          }
+          .label-yes {
+            right: 70%; 
+            top: -5%;
+          }
+          .row {
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+          }
+          .column {
+            flex: 0 0 auto;
+          }
+          .bff {
+            max-width: 48px;
+          }
+          .pxf {
+            max-width: 70px;
+          }
+        ")),
         ),
-        div(
-          class = "flowchart-container",
-          # Start node
-          div(
-            class = "circle",
-            style = "top: 20px; left: 20px;", "Start"
-          ),
+        div(class = "flowchart tree",
+        tags$ul(
+          tags$li(
+            tags$a(href = "#", class = "decision",
+                  div(class = "icon", tags$i(class = "fas fa-question-circle")),
+                  "Do you have data?"
+            ),
+            tags$ul(
+              tags$li(
+                div(class = "label label-no", "No"),
+                tags$a(href = "#", class = "decision",
+                      div(class = "icon", tags$i(class = "fas fa-wand-magic-sparkles")),
+                      "Use our utilities to get data"
+                ),
+                tags$ul(
+                  tags$li(tags$a(href = "#", class = "process",
+                                div(class = "icon", tags$i(class = "fas fa-flask")),
+                                "Simulate BFF/PXF"
+                  )),
+                  tags$li(tags$a(href = "#", class = "process",
+                                div(class = "icon", tags$i(class = "fas fa-hand-pointer")),
+                                "Get example data"
+                  ))
+                )
+              ),
+              tags$li(
+                div(class = "label label-yes", "Yes"),
+                tags$a(href = "#", class = "decision",
+                      div(class = "icon", tags$i(class = "fas fa-file-alt")),
+                      "Which file type?"
+                ),
+                tags$ul(
+                  tags$li(
+                    tags$a(href = "#", class = "process",
+                          div(class = "row",
+                              div(class = "column",
+                                  tags$img(src = "https://avatars.githubusercontent.com/u/33450937?s=48&v=4", class = "bff")
+                              ),
+                              div(class = "column",
+                                  tags$img(src = "https://pubcasefinder.dbcls.jp/static/images/pcf/top/logo_phenopackets.png", class = "pxf")
+                              )
+                          ),
+                          "BFF/PXF"
+                    )
+                  ),
+                  tags$li(
+                    tags$a(href = "#", class = "process",
+                          div(class = "icon", tags$i(class = "fas fa-file-csv")),
+                          "generic CSV"
+                    )
+                  ),
+                  tags$li(
+                    tags$a(href = "#", class = "process",
+                          div(class = "icon", tags$i(class = "fas fa-file")),
+                          HTML("RedCap,<br/>OMOP-CDM,<br/>CDISC-ODM<br/>")
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )),
+        # div(
+        #   class = "flowchart-container",
+        #   # Start node
+        #   div(
+        #     class = "circle",
+        #     style = "top: 20px; left: 20px;", "Start"
+        #   ),
 
-          # Line from Start to A
-          div(
-            class = "line vertical",
-            style = "top: 120px; left: 20px; height: 80px;"
-          ),
-          div(
-            class = "arrow down",
-            style = "top: 200px; left: 20px;"
-          ),
+        #   # Line from Start to A
+        #   div(
+        #     class = "line vertical",
+        #     style = "top: 120px; left: 20px; height: 80px;"
+        #   ),
+        #   div(
+        #     class = "arrow down",
+        #     style = "top: 200px; left: 20px;"
+        #   ),
 
-          # Node A
-          div(
-            class = "diamond", style = "top: 200px; left: 350px;",
-            div(class = "diamond-text", "Is input data available?")
-          ),
+        #   # Node A
+        #   div(
+        #     class = "diamond", style = "top: 200px; left: 350px;",
+        #     div(class = "diamond-text", "Is input data available?")
+        #   ),
 
-          # Line from A to B ('No' path)
-          div(class = "line vertical", style = "top: 300px; left: 400px; height: 80px;"),
-          div(class = "arrow down", style = "top: 380px; left: 390px;"),
-          div(class = "label", style = "top: 330px; left: 410px;", "No"),
+        #   # Line from A to B ('No' path)
+        #   div(class = "line vertical", style = "top: 300px; left: 400px; height: 80px;"),
+        #   div(class = "arrow down", style = "top: 380px; left: 390px;"),
+        #   div(class = "label", style = "top: 330px; left: 410px;", "No"),
 
-          # Node B
-          div(
-            class = "diamond", style = "top: 380px; left: 350px;",
-            div(class = "diamond-text", "Which data source?")
-          ),
+        #   # Node B
+        #   div(
+        #     class = "diamond", style = "top: 380px; left: 350px;",
+        #     div(class = "diamond-text", "Which data source?")
+        #   ),
 
-          # Line from A to G ('Yes' path)
-          div(class = "line horizontal", style = "top: 250px; left: 400px; width: 150px;"),
-          div(class = "arrow right", style = "top: 240px; left: 550px;"),
-          div(class = "label", style = "top: 230px; left: 475px;", "Yes"),
+        #   # Line from A to G ('Yes' path)
+        #   div(class = "line horizontal", style = "top: 250px; left: 400px; width: 150px;"),
+        #   div(class = "arrow right", style = "top: 240px; left: 550px;"),
+        #   div(class = "label", style = "top: 230px; left: 475px;", "Yes"),
 
-          # Node G
-          div(
-            class = "diamond", style = "top: 200px; left: 600px;",
-            div(class = "diamond-text", "Which file type?")
-          ),
+        #   # Node G
+        #   div(
+        #     class = "diamond", style = "top: 200px; left: 600px;",
+        #     div(class = "diamond-text", "Which file type?")
+        #   ),
 
-          # Line from B to D (Left)
-          div(class = "line horizontal", style = "top: 430px; left: 350px; width: -150px; transform: scaleX(-1);"),
-          div(class = "arrow left", style = "top: 420px; left: 200px;"),
+        #   # Line from B to D (Left)
+        #   div(class = "line horizontal", style = "top: 430px; left: 350px; width: -150px; transform: scaleX(-1);"),
+        #   div(class = "arrow left", style = "top: 420px; left: 200px;"),
 
-          # Node D
-          div(class = "rectangle", style = "top: 400px; left: 50px;", "Example data"),
+        #   # Node D
+        #   div(class = "rectangle", style = "top: 400px; left: 50px;", "Example data"),
 
-          # Line from B to C (Right)
-          div(class = "line horizontal", style = "top: 430px; left: 400px; width: 200px;"),
-          div(class = "arrow right", style = "top: 420px; left: 600px;"),
+        #   # Line from B to C (Right)
+        #   div(class = "line horizontal", style = "top: 430px; left: 400px; width: 200px;"),
+        #   div(class = "arrow right", style = "top: 420px; left: 600px;"),
 
-          # Node C
-          div(class = "rectangle", style = "top: 400px; left: 600px;", "Simulated BFF/PXF"),
+        #   # Node C
+        #   div(class = "rectangle", style = "top: 400px; left: 600px;", "Simulated BFF/PXF"),
 
-          # Line from G to H (Up)
-          div(class = "line vertical", style = "top: 150px; left: 650px; height: -80px;"),
-          div(class = "arrow up", style = "top: 70px; left: 640px;"),
+        #   # Line from G to H (Up)
+        #   div(class = "line vertical", style = "top: 150px; left: 650px; height: -80px;"),
+        #   div(class = "arrow up", style = "top: 70px; left: 640px;"),
 
-          # Node H
-          div(class = "rectangle", style = "top: 20px; left: 600px;", "CSV"),
+        #   # Node H
+        #   div(class = "rectangle", style = "top: 20px; left: 600px;", "CSV"),
 
-          # Line from G to F (Down)
-          div(class = "line vertical", style = "top: 300px; left: 650px; height: 100px;"),
-          div(class = "arrow down", style = "top: 400px; left: 640px;"),
+        #   # Line from G to F (Down)
+        #   div(class = "line vertical", style = "top: 300px; left: 650px; height: 100px;"),
+        #   div(class = "arrow down", style = "top: 400px; left: 640px;"),
 
-          # Node F
-          div(class = "rectangle", style = "top: 400px; left: 600px;", "BFF/PXF")
-        ),
+        #   # Node F
+        #   div(class = "rectangle", style = "top: 400px; left: 600px;", "BFF/PXF")
+        # ),
         p("
           If you do not have Phenotypic data stored in
           Beacon-friendly format (BFF) or Phenopackets v2(PXF),
