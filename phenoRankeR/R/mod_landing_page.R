1d57f612d98a4e4210e269e80406dcfd595fbed8
Ivo Leist <ivo.leist@googlemail.com>
2024-09-28 16:31:12 +0200

    removed code duplication by adding a create_mode _card function

--- phenoRankeR/R/mod_landing_page.R
+++ phenoRankeR/R/mod_landing_page.R
@@ -14,14 +14,29 @@
 # because it was not possible to run updateNavbarPage from this module
 # the function is called from app_server.R
 
+# Utility function to create a card with an action button and image
+create_mode_card <- function(ns, area, header, button_id, button_text, img_src) {
+  grid_card(
+    area = area,
+    card_header(header),
+    card_body(
+      padding = "5px",
+      actionButton(
+        ns(button_id),
+        button_text,
+        style = "background-color: #1976d2; color: white;"
+      ),
+      img(src = img_src, style = "height: 200px;")
+    )
+  )
+}
+
 old_layout <- c(
   "         650px       1fr     ",
   "1fr      welcome     welcome ",
   "800px    new_user    modes   ",
   "5px      version     version "
 )
-
-
 
 mod_landing_page_ui <- function(id) {
   ns <- NS(id)
@@ -39,42 +54,30 @@ mod_landing_page_ui <- function(id) {
     gap_size = "0px",
     grid_place(
       area = "welcome",
-      card_body(
-        style = "text-align: center;",
+      div(
         h3("Welcome to Pheno-Ranker"),
         span(
           "Your interactive tool for semantic similarity analyses
           of Phenotypic Data Stored in GA4GH Standards and Beyond"
-        )
+        ),
+        style = "margin-bottom: 0px; text-align: center;"
       )
     ),
-    grid_card(
-      area = "cohort_mode",
-      card_header("Intra-/Inter-cohort comparison"),
-      card_body(
-        actionButton(
-          ns("navigateToCohortMode2"),
-          "Go to Cohort comparison",
-        ),
-        img(
-          src = "www/images/cohort_mode_icon.svg",
-          style = "width: 100%;"
-        )
-      )
+    create_mode_card(
+      ns,
+      "cohort_mode",
+      "Intra-/Inter-cohort comparison",
+      "navigateToCohortMode2",
+      "Go to Cohort comparison",
+      "www/images/cohort_mode_icon.svg"
     ),
-    grid_card(
-      area = "patient_mode",
-      card_header("Patient vs Reference Cohort(s)"),
-      card_body(
-        actionButton(
-          ns("navigateToPatientMode2"),
-          "Go to Patient vs Reference Cohort(s) comparison",
-        ),
-        img(
-          src = "www/images/patient_mode_icon.svg",
-          style = "width: 100%;"
-        )
-      )
+    create__mode_card(
+      ns,
+      "patient_mode",
+      "Patient vs Reference Cohort(s)",
+      "navigateToPatientMode2",
+      "Go to Patient vs Reference Cohort(s) comparison",
+      "www/images/patient_mode_icon.svg"
     ),
     grid_card(
       area = "new_user",
@@ -84,157 +87,6 @@ mod_landing_page_ui <- function(id) {
         ns
       )
     ),
-    # grid_card(
-    #   area = "modes",
-    #   card_header("Jump right in!"),
-    #   card_body(
-    #     grid_container(
-    #       layout = c(
-    #         "         500px           20px            1fr           1fr             ",
-    #         "20px     explanation     explanation     explanation   explanation     ",
-    #         "40px     patient_btn     separator       cohort_btn    cohort_btn      ",
-    #         "300px    patient_svg     separator       intra_title   intra_cohort_svg",
-    #         "300px    patient_svg     separator       inter_title   inter_cohort_svg"
-    #       ),
-    #       gap_size = "0px",
-    #       grid_place(
-    #         area = "explanation",
-    #         p(
-    #           "Pheno-Ranker can be used in two different modes: ",
-    #         )
-    #       ),
-    #       grid_place(
-    #         area = "separator",
-    #         div(
-    #           style = "border-left: 5px solid grey; margin-top: 50px; margin-left: 7px;"
-    #         )
-    #       ),
-    #       grid_place(
-    #         area = "patient_btn",
-    #         actionButton(
-    #           ns("navigateToPatientMode2"),
-    #           "Go to Patient vs Reference Cohort(s) comparison",
-    #         )
-    #       ),
-    #       grid_place(
-    #         area = "cohort_btn",
-    #         actionButton(
-    #           ns("navigateToCohortMode2"),
-    #           "Go to Cohort comparison",
-    #         )
-    #       ),
-    #       grid_place(
-    #         area = "patient_svg",
-    #         tags$img(
-    #           src = "www/images/patient_mode_representation.svg",
-    #           style = "padding-top: 60px;"
-    #         )
-    #       ),
-    #       grid_place(
-    #         area = "intra_title",
-    #         h3(
-    #           "Intra-cohort",
-    #           style = "padding-top: 140px; text-align: center;"
-    #         )
-    #       ),
-    #       grid_place(
-    #         area = "intra_cohort_svg",
-    #         tags$img(
-    #           # src = "www/images/patient_mode_representation.svg",
-    #           src = "www/images/intra_cohort_mode_representation.svg",
-    #           style = "height: 320px;"
-    #         )
-    #       ),
-    #       grid_place(
-    #         area = "inter_title",
-    #         h3(
-    #           "Inter-cohort",
-    #           style = "padding-top: 140px; text-align: center;"
-    #         )
-    #       ),
-    #       grid_place(
-    #         area = "inter_cohort_svg",
-    #         tags$img(
-    #           # src = "www/images/patient_mode_representation.svg",
-    #           src = "www/images/inter_cohort_mode_representation.svg",
-    #           style = "height: 320px;"
-    #         )
-    #       )
-    #     )
-    #   )
-
-
-    #   grid_card(
-    #     area = "patient",
-    #     card_header("Patient vs Reference Cohort(s)"),
-    #     card_body(
-    #       p(
-    #         "Rank all the individuals in your reference cohort according to their similarity to your patient."
-    #       )
-    #     )
-    #   ),
-    #   grid_card(
-    #     area = "cohort",
-    #     card_header("Intra-/Inter-cohort comparison"),
-    #     card_body(
-    #       p(
-    #         "Rank all the individuals in your reference cohort according to their similarity to each other."
-    #       )
-    #     )
-    #   )
-    # ),
-
-    #   p(
-    #     "Pheno-Ranker can be used in two different modes: "
-    #   ),
-    #   tags$img(
-    #     src = "www/images/patient_mode_representation.svg",
-    #     style = "width: 100%;"
-    #   ),
-    #   h6("Mode 1: Compare your patient to one or multiple reference cohorts"),
-    #   p("
-    #     Rank all the individuals in your
-    #     reference cohort according to their similarity to your patient.
-    #   "),
-    #   actionButton(
-    #     ns("navigateToPatientMode"),
-    #     "Patient vs Reference Cohort(s)",
-    #     style = "width: 100%;"
-    #   ),
-    #   hr(),
-    #   h6("Mode 2: Intra-/Inter-cohort comparison"),
-    #   grid_container(
-    #     layout = c(
-    #       "         1fr         1fr   ",
-    #       "150px    intra       inter "
-    #     ),
-    #     grid_card(
-    #       area = "intra",
-    #       card_header("Intra-cohort comparison"),
-    #       card_body(
-    #         p("
-    #           Rank all the individuals in your
-    #           reference cohort according to their similarity to each other.
-    #         ")
-    #       )
-    #     ),
-    #     grid_card(
-    #       area = "inter",
-    #       card_header("Inter-cohort comparison"),
-    #       card_body(
-    #         p("
-    #           Compare two or more cohorts with each other.
-    #         ")
-    #       )
-    #     )
-    #   ),
-    #   actionButton(
-    #     ns("navigateToCohortMode"),
-    #     "Intra-/Inter-cohort comparison",
-    #     style = "width: 100%;"
-    #   )
-    # )
-    # ),
     grid_place(
       area = "version",
       card_body(
